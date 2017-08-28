faeee8c9469ccd503d2f47b0387ab49bbd7d9779
Caio Souza <souzacaio20@gmail.com>
2017-08-27 22:33:54 -0300

    testa ai

--- service/app/Http/Controllers/TwitterController.php
+++ service/app/Http/Controllers/TwitterController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use TwitterOAuth\Auth\SingleUserAuth as TwitterOAuth;
 use TwitterOAuth\Serializer\ArraySerializer as TwitterSerializer;
+use Illuminate\Http\Request;
 
 class TwitterController extends Controller
 {
@@ -14,9 +15,10 @@ class TwitterController extends Controller
         'oauth_token_secret' => 'NvsvSzepJg0XuAocSa7O5tUamBXSufRqNg8wE73VNvjpx'
     ];
 
-    public function index($term="")
+    public function index(Request $request)
     {
-        $term = "Secretaria Civil";
+        //$term = "Secretaria Civil";
+        $term = $request->input("term") ? : "Secretaria Civil";
         $tweet = $this->searchTwitter($term);
         return ($tweet) ? $tweet:"";
 
